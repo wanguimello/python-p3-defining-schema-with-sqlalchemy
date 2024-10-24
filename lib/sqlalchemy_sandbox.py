9b132cb3572ec432285ac1fbe226edc13a389692
joycewangui <joycewangui590@gmail.com>
2024-10-24 20:36:44 +0300

    sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a base class for declarative class definitions
 Base = declarative_base()
 
+#creating the table
+# Define your data model as a class
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
 
 if __name__ == '__main__':
-    pass
+    #persisting/saving the table
+    # Create a SQLite database engine
+    engine = create_engine('sqlite:///students.db')
+    # Create the database schema
+    Base.metadata.create_all(engine)
